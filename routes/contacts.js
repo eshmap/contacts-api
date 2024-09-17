1ef56fa77daf6327883bece48cb1865aad5c709d
Serenity Mapatac <w0495643@nscc.ca>
2024-09-17 11:41:17 -0300

    Lab 1 complete

---
+++ routes/contacts.js
@@ -0,0 +1,38 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Contacts route');
+});
+
+// Get all contacts
+router.get('/all', (req, res) => {
+    res.send('All contacts');
+  });
+  
+  // Get a contact by id
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    res.send('Contact by id ' + id);
+  });
+  
+// make a new contact
+router.post('/create', (req, res) => {
+    const newContact = req.body;
+    res.send('Contact created: ' + JSON.stringify(newContact));
+  });
+  
+  // Update an existing contact
+  router.put('/update', (req, res) => {
+    const updatedContact = req.body;
+    res.send('Contact updated: ' + JSON.stringify(updatedContact));
+  });
+  
+  // Delete a contact
+  router.delete('/delete', (req, res) => {
+    const contactId = req.body.id;
+    res.send('Contact deleted with ID: ' + contactId);
+  });
+
+export default router;
